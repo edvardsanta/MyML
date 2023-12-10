beb96f4bf6607b704510b60e7c4d45c61d12433b
EDU-COMPUTER\Eduardo <eduardosantamarco123@outlook.com>
2023-12-10 15:04:55 -0300

    Create NaiveBayesClassifier
    - This abstract class is specially yo populate dictionaries

---
+++ Abstracts/NaiveBayesClassifier.cs
@@ -0,0 +1,48 @@
+﻿using MyML.Interfaces;
+
+namespace MyML.Abstracts
+{
+    public abstract class NaiveBayesClassifier : INaiveBayesClassifier
+    {
+        protected Dictionary<string, Dictionary<string, int>> wordCountsPerLabel;
+        protected Dictionary<string, int> totalWordsPerLabel;
+
+        public NaiveBayesClassifier()
+        {
+            wordCountsPerLabel = new Dictionary<string, Dictionary<string, int>>();
+            totalWordsPerLabel = new Dictionary<string, int>();
+        }
+
+        public virtual void Train(IEnumerable<ClassifierModel> trainingData)
+        {
+            foreach (var data in trainingData)
+            {
+                // Initialize dictionary for new labels
+                if (!wordCountsPerLabel.ContainsKey(data.Label))
+                {
+                    wordCountsPerLabel[data.Label] = new Dictionary<string, int>();
+                    totalWordsPerLabel[data.Label] = 0;
+                }
+
+                Dictionary<string, int> targetDictionary = wordCountsPerLabel[data.Label];
+                IEnumerable<string> words = Tokenize(data.Text);
+
+                foreach (var word in words)
+                {
+                    if (!targetDictionary.ContainsKey(word))
+                        targetDictionary[word] = 0;
+                    targetDictionary[word]++;
+                }
+
+                totalWordsPerLabel[data.Label]++;
+            }
+        }
+
+        protected IEnumerable<string> Tokenize(string text, char separator = ' ')
+        {
+            return text.ToLower().Split(separator);
+        }
+       
+        public abstract string Predict(string text);    
+    }
+}
